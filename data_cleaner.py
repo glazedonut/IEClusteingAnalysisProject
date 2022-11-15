53b7a5eeb96c33d53ed43b138f709b3a9809d61e
glazedonut <40008795+glazedonut@users.noreply.github.com>
2022-11-15 14:39:45 -0500

    Updated Data Cleaning Method. Pull whenever. NO CHANGE TO REPORT.IPYNB

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,12 +6,16 @@ Created on Tue Nov 15 10:51:10 2022
 """
 
 import pandas as pd
-
+import numpy as np
 world=pd.read_csv('data/World Indicators.csv')
-world=world.dropna()
+
 world['Business Tax Rate']=world['Business Tax Rate'].str.rstrip("%").astype('float')/100
 world['GDP'] = world['GDP'].str.replace(',', '')
 world['GDP'] = world['GDP'].str.replace('$', '')
 world['GDP']=world['GDP'].astype('float')
 world['Health Exp/Capita']=world['GDP'].replace('$', '').astype('float')
+for i in range(len(world.columns.values)-2):
+    world[world.columns.values[i]]=world[world.columns.values[i]].replace(np.NaN,world[world.columns.values[i]].mean())
+print(world.isnull().sum())    
 world.to_csv('data/World Indicators-clean.csv')
+
